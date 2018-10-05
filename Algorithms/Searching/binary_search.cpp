76a2bad5d6606ea06f4aa41e6d8eb70dec8aa253
manik dhupar <manik.dhupar7@gmail.com>
2018-10-05 22:31:56 +0530

    added binary_search

---
+++ Algorithms/Searching/binary_search.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int BinarySearch(int arr[], int n, int val)
+{
+  int start = 0, end = n - 1;
+  while (start <= end)
+  {
+    int mid = (start + end) / 2;
+    if (val == arr[mid])
+    {
+      return mid;
+    }
+    else if (val > arr[mid])
+    {
+      start = mid + 1;
+    }
+    else
+    {
+      end = mid - 1;
+    }
+  }
+  return -1;
+}
+
+int main()
+{
+
+  int size, val;
+  cin >> size;
+  int *input = new int[1 + size];
+
+  for (int i = 0; i < size; i++)
+    cin >> input[i];
+
+  cin >> val;
+  cout << BinarySearch(input, size, val);
+  return 0;
+}
