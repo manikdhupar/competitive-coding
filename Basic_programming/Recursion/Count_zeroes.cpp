11ed215325b3fa100fa3c1a815796130148ada67
manik dhupar <manik.dhupar7@gmail.com>
2018-10-17 03:46:59 +0530

    added count zeroes

---
+++ Basic_programming/Recursion/Count_zeroes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int countZeros(int n)
+{
+  if (n == 0)
+  {
+    return 0;
+  }
+
+  int ans = countZeros(n / 10);
+  if (n % 10 == 0)
+  {
+    return ans + 1;
+  }
+  else
+  {
+    return ans;
+  }
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  cout << countZeros(n) << endl;
+}
