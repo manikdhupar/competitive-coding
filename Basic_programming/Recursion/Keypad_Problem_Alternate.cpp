3e5f0cce65c9b32284de0baa3e701d66d22dd481
manik dhupar <manik.dhupar7@gmail.com>
2018-10-25 00:05:35 +0530

    modified keypad problem

--- Basic_programming/Recursion/Keypad_Problem_Alternate.cpp
+++ Basic_programming/Recursion/Keypad_Problem_Alternate.cpp
@@ -2,6 +2,8 @@
 #include <string>
 using namespace std;
 
+string keypadArray[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
 void printKeypad(int num, string output)
 {
   // base case
@@ -14,63 +16,69 @@ void printKeypad(int num, string output)
   int digit = num % 10;
   num = num / 10;
 
-  if (digit == 2)
+  for (int i = 0; i < keypadArray[digit].length(); i++)
   {
-    printKeypad(num, 'a' + output);
-    printKeypad(num, 'b' + output);
-    printKeypad(num, 'c' + output);
+    printKeypad(num, keypadArray[digit][i] + output);
   }
 
-  else if (digit == 3)
-  {
-    printKeypad(num, 'd' + output);
-    printKeypad(num, 'e' + output);
-    printKeypad(num, 'f' + output);
-  }
+  // For understanding
+  // if (digit == 2)
+  // {
+  //     printKeypad(num, 'a' + output);
+  //     printKeypad(num, 'b' + output);
+  //     printKeypad(num, 'c' + output);
+  // }
 
-  else if (digit == 4)
-  {
-    printKeypad(num, 'g' + output);
-    printKeypad(num, 'h' + output);
-    printKeypad(num, 'i' + output);
-  }
+  // else if (digit == 3)
+  // {
+  //     printKeypad(num, 'd' + output);
+  //     printKeypad(num, 'e' + output);
+  //     printKeypad(num, 'f' + output);
+  // }
 
-  else if (digit == 5)
-  {
-    printKeypad(num, 'j' + output);
-    printKeypad(num, 'k' + output);
-    printKeypad(num, 'l' + output);
-  }
+  // else if (digit == 4)
+  // {
+  //     printKeypad(num, 'g' + output);
+  //     printKeypad(num, 'h' + output);
+  //     printKeypad(num, 'i' + output);
+  // }
 
-  else if (digit == 6)
-  {
-    printKeypad(num, 'm' + output);
-    printKeypad(num, 'n' + output);
-    printKeypad(num, 'o' + output);
-  }
+  // else if (digit == 5)
+  // {
+  //     printKeypad(num, 'j' + output);
+  //     printKeypad(num, 'k' + output);
+  //     printKeypad(num, 'l' + output);
+  // }
 
-  else if (digit == 7)
-  {
-    printKeypad(num, 'p' + output);
-    printKeypad(num, 'q' + output);
-    printKeypad(num, 'r' + output);
-    printKeypad(num, 's' + output);
-  }
+  // else if (digit == 6)
+  // {
+  //     printKeypad(num, 'm' + output);
+  //     printKeypad(num, 'n' + output);
+  //     printKeypad(num, 'o' + output);
+  // }
 
-  else if (digit == 8)
-  {
-    printKeypad(num, 't' + output);
-    printKeypad(num, 'u' + output);
-    printKeypad(num, 'v' + output);
-  }
+  // else if (digit == 7)
+  // {
+  //     printKeypad(num, 'p' + output);
+  //     printKeypad(num, 'q' + output);
+  //     printKeypad(num, 'r' + output);
+  //     printKeypad(num, 's' + output);
+  // }
 
-  else if (digit == 9)
-  {
-    printKeypad(num, 'w' + output);
-    printKeypad(num, 'x' + output);
-    printKeypad(num, 'y' + output);
-    printKeypad(num, 'z' + output);
-  }
+  // else if (digit == 8)
+  // {
+  //     printKeypad(num, 't' + output);
+  //     printKeypad(num, 'u' + output);
+  //     printKeypad(num, 'v' + output);
+  // }
+
+  // else if (digit == 9)
+  // {
+  //     printKeypad(num, 'w' + output);
+  //     printKeypad(num, 'x' + output);
+  //     printKeypad(num, 'y' + output);
+  //     printKeypad(num, 'z' + output);
+  // }
 }
 
 void printKeypad(int num)
