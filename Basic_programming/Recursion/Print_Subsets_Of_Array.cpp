d6db97dd2f6bbcd4db46b888e58cf527b1089395
manik dhupar <manik.dhupar7@gmail.com>
2018-10-31 01:41:31 +0530

    added subsets of array using backtracking

---
+++ Basic_programming/Recursion/Print_Subsets_Of_Array.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void helper(int input[], int inputSize, int output[], int outputSize)
+{
+  if (inputSize == 0)
+  {
+    for (int i = 0; i < outputSize; i++)
+    {
+      cout << output[i] << " ";
+    }
+    cout << endl;
+    return;
+  }
+
+  helper(input + 1, inputSize - 1, output, outputSize);
+  int element = input[0];
+  int *array = new int[inputSize + 1];
+  int i;
+  for (i = 0; i < outputSize; i++)
+  {
+    array[i] = output[i];
+  }
+  array[i] = element;
+
+  helper(input + 1, inputSize - 1, array, outputSize + 1);
+}
+
+void printSubsetsOfArray(int input[], int size)
+{
+  int *array = new int[1000];
+  helper(input, size, array, 0);
+}
+
+int main()
+{
+  int input[1000], length;
+  cin >> length;
+  for (int i = 0; i < length; i++)
+    cin >> input[i];
+  printSubsetsOfArray(input, length);
+}
