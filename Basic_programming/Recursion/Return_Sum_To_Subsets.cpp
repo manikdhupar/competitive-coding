e6fd30fc5bb57f4f621cf342198fe5a3e1936b9e
manik dhupar <manik.dhupar7@gmail.com>
2018-11-07 20:34:30 +0530

    modifies return subset sum to k

--- Basic_programming/Recursion/Return_Sum_To_Subsets.cpp
+++ Basic_programming/Recursion/Return_Sum_To_Subsets.cpp
@@ -1,62 +1,54 @@
 #include <iostream>
 using namespace std;
 
-int subsetSumToK(int input[], int n, int output[][50], int k, int length, int finalOutput[][50])
+int subsetSumToK(int input[], int n, int output[][50], int k)
 {
 
   if (n == 0)
   {
-    output[0][0] = 0;
-    return 1;
-  }
-
-  int smallOutput = subsetSumToK(input + 1, n - 1, output, k, length, finalOutput);
-
-  for (int i = 0; i < smallOutput; i++)
-  {
-    output[i + smallOutput][0] = output[i][0] + 1;
-    output[i + smallOutput][1] = input[0];
-    for (int j = 1; j <= output[i][0]; j++)
+    if (k == 0)
     {
-      output[i + smallOutput][j + 1] = output[i][j];
+      output[0][0] = 0;
+      return 1;
+    }
+    else
+    {
+      return 0;
     }
   }
 
-  if (n == length)
-  {
-    int count = 0;
-    for (int i = 0; i < 2 * smallOutput; i++)
-    {
-      int sum = 0;
-      for (int j = 1; j <= output[i][0]; j++)
-      {
-        sum += output[i][j];
-      }
-      if (sum == k)
-      {
-        finalOutput[count][0] = output[i][0];
-        for (int b = 1; b <= finalOutput[count][0]; b++)
-        {
-          finalOutput[count][b] = output[i][b];
-        }
-        count++;
-      }
-    }
-    return count;
-  }
-  return 2 * smallOutput;
-}
+  int output1[1000][50], output2[1000][50];
 
-int subsetSumToK(int input[], int n, int output[][50], int k)
-{
-  int finalOutput[10000][50];
-  int length = n;
-  return subsetSumToK(input, n, finalOutput, k, length, output);
+  int s1 = subsetSumToK(input + 1, n - 1, output1, k - input[0]);
+  int s2 = subsetSumToK(input + 1, n - 1, output2, k);
+
+  int row = 0;
+  for (int i = 0; i < s1; i++)
+  {
+    output[row][0] = output1[i][0] + 1;
+    output[row][1] = input[0];
+    for (int j = 1; j <= output1[i][0]; j++)
+    {
+      output[row][j + 1] = output1[i][j];
+    }
+    row++;
+  }
+
+  for (int i = 0; i < s2; i++)
+  {
+    for (int j = 0; j <= output2[i][0]; j++)
+    {
+      output[row][j] = output2[i][j];
+    }
+    row++;
+  }
+
+  return row;
 }
 
 int main()
 {
-  int input[20], length, output[10000][50], k;
+  int input[20], length, output[1000][50], k;
   cin >> length;
   for (int i = 0; i < length; i++)
     cin >> input[i];
