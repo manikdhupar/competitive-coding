8ac2f5f4ded9500dc7c7210845d94fce42c94b64
manik dhupar <manik.dhupar7@gmail.com>
2018-12-26 15:36:07 +0530

    add is identical

---
+++ Data_Structures/Trees/IsIdentical.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+template <typename T>
+class TreeNode
+{
+public:
+  T data;
+  vector<TreeNode<T> *> children;
+
+  TreeNode(T data)
+  {
+    this->data = data;
+  }
+
+  ~TreeNode()
+  {
+    for (int i = 0; i < children.size(); i++)
+    {
+      delete children[i];
+    }
+  }
+};
+
+#include <queue>
+
+TreeNode<int> *takeInputLevelWise()
+{
+  int rootData;
+  cin >> rootData;
+  TreeNode<int> *root = new TreeNode<int>(rootData);
+
+  queue<TreeNode<int> *> pendingNodes;
+
+  pendingNodes.push(root);
+  while (pendingNodes.size() != 0)
+  {
+    TreeNode<int> *front = pendingNodes.front();
+    pendingNodes.pop();
+    int numChild;
+    cin >> numChild;
+    for (int i = 0; i < numChild; i++)
+    {
+      int childData;
+      cin >> childData;
+      TreeNode<int> *child = new TreeNode<int>(childData);
+      front->children.push_back(child);
+      pendingNodes.push(child);
+    }
+  }
+  return root;
+}
+
+bool isIdentical(TreeNode<int> *root1, TreeNode<int> *root2)
+{
+  if (root1->data == root2->data && root1->children.size() == root2->children.size())
+  {
+    bool one;
+    for (int i = 0; i < root1->children.size(); i++)
+    {
+      one = isIdentical(root1->children[i], root2->children[i]);
+      if (!one)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+int main()
+{
+  TreeNode<int> *root1 = takeInputLevelWise();
+  TreeNode<int> *root2 = takeInputLevelWise();
+  if (isIdentical(root1, root2))
+  {
+    cout << "true" << endl;
+  }
+  else
+  {
+    cout << "false" << endl;
+  }
+}
