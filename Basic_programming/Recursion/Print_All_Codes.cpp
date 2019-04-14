3341bf640d01b87b42ddbcb20a2e19b439413d93
manik dhupar <manik.dhupar7@gmail.com>
2019-04-14 15:24:49 +0530

    added print all codes

---
+++ Basic_programming/Recursion/Print_All_Codes.cpp
@@ -0,0 +1,62 @@
+// Assume that the value of a = 1, b = 2, c = 3, ... , z = 26. You are given a numeric string S. Write a program to print the list of all possible codes that can be generated from the given string.
+// Note : The order of codes are not important. Just print them in different lines.
+// Input format :
+// A numeric string S
+// Output Format :
+// All possible codes in different lines
+// Constraints :
+// 1 <= Length of String S <= 10
+// Sample Input:
+// 1123
+// Sample Output:
+// aabc
+// kbc
+// alc
+// aaw
+// kw
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ref = "0abcdefghijklmnopqrstuvwxyz";
+
+void helper(string input, string output)
+{
+  if (input.size() == 0)
+  {
+    cout << output << endl;
+    return;
+  }
+
+  int temp1 = input[0] - 48;
+  helper(input.substr(1), output + ref[temp1]);
+  if (input.size() > 1)
+  {
+    string s;
+    s += input[0];
+    s += input[1];
+    int temp2 = stoi(s);
+    if (temp2 <= 26)
+    {
+      helper(input.substr(2), output + ref[temp2]);
+    }
+  }
+}
+
+void printAllPossibleCodes(string input)
+{
+  /*
+    Given the input as a string, print all its possible combinations. You do not need to return anything.
+    */
+  helper(input, "");
+}
+
+int main()
+{
+  string input;
+  cin >> input;
+
+  printAllPossibleCodes(input);
+  return 0;
+}
